f69d1885040e157a58f54afcc40088af5f2fd920
Karol <karolpoczta@gmail.com>
2020-04-13 17:52:37 +0200

    new champion details screen

--- lib/ChampionDetails.dart
+++ lib/ChampionDetails.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
 
+import 'ChampionDetailItems.dart';
+import 'ChampionDetailOverview.dart';
+import 'ChampionDetailStatistics.dart';
+
 // ignore: must_be_immutable
 class ChampionDetails extends StatelessWidget {
 
   var championDetailsData;
-
   ChampionDetails(this.championDetailsData);
 
+  List<Widget> containers = [
+    ChampionDetailOverview(),
+    ChampionDetailStatistics(),
+    ChampionDetailItems()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(championDetailsData.name),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(championDetailsData.name),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'informacje',
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                text: 'statystyki',
+                icon: Icon(Icons.list),
+              ),
+              Tab(
+                text: 'przedmioty',
+                icon: Icon(Icons.grain),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: containers,
+        ),
       ),
-      body: ListView.builder(
-        itemCount: championDetailsData.stats.length,
-        itemBuilder: (context, index) {
-          String key = championDetailsData.stats.keys.elementAt(index);
-          return Text(
-              key+": "+championDetailsData.stats[key].toString()
-          );
-        }),
     );
   }
 }
