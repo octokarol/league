1c098d5d124a323a5df2879b73a92cc05e9c2217
Karol <karolpoczta@gmail.com>
2020-05-14 13:09:57 +0200

    spells page done

--- lib/ChampionDetailStatistics.dart
+++ lib/ChampionDetailStatistics.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:league/ChampionDetailsJsonData.dart';
-import 'JsonData.dart';
 
 class ChampionDetailStatistics extends StatelessWidget {
   var championDetailsData;
